0fa666e03e624043719bf39ec9f9f89523e56b78
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2021-11-23 10:27:42 -0500

    004 Dealing With Form Submission & Getting User Input Values
    demo two approaches to input data
    1, use onChange to trace each key stroke, and update state.
    2, use useRef() to get the reference of the Input Element and take the value
    
    Which one to choose depends on your requirement

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,14 +1,20 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 const SimpleInput = (props) => {
+  // if use only one time, and don't need check directly, REF would be a good choose
   console.log("SimpleInput");
   const [enteredName, setEnteredName] = useState("");
+  const nameInputRef = useRef();
   const nameChangeHandler = (event) => {
     setEnteredName(event.target.value);
   };
   const submitHandler = (event) => {
     event.preventDefault();
     console.log(enteredName);
+    setEnteredName(""); // reset the input
+    var result = nameInputRef.current.value; // get value with REF
+    //nameInputRef.current.value="";   // legal, but it will manipulate DOM, let REACT to do it
+    console.log("ref=", result);
   };
 
   return (
@@ -20,6 +26,7 @@ const SimpleInput = (props) => {
           id="name"
           value={enteredName}
           onChange={nameChangeHandler}
+          ref={nameInputRef}
         />
       </div>
       <div className="form-actions">

21152b2a1ad9bcb85ad74b4acf60ad06f6bec040
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2021-11-23 10:09:48 -0500

    004-approach one-valid after each stroke

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,9 +1,26 @@
+import { useState } from "react";
+
 const SimpleInput = (props) => {
+  console.log("SimpleInput");
+  const [enteredName, setEnteredName] = useState("");
+  const nameChangeHandler = (event) => {
+    setEnteredName(event.target.value);
+  };
+  const submitHandler = (event) => {
+    event.preventDefault();
+    console.log(enteredName);
+  };
+
   return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+    <form onSubmit={submitHandler}>
+      <div className="form-control">
+        <label htmlFor="name">Your Name</label>
+        <input
+          type="text"
+          id="name"
+          value={enteredName}
+          onChange={nameChangeHandler}
+        />
       </div>
       <div className="form-actions">
         <button>Submit</button>

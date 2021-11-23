9955a384bb03027b4d04ea7109ee2ee403ddbb95
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2021-11-23 14:35:18 -0500

    010 Managing The Overall Form Validity
    refactor code
    it is recommended that we can derive state from other state other than use too many hooks.
    
    we delete useEffect and formIsValid hook

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,21 +1,17 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
 const SimpleInput = (props) => {
   const [enteredName, setEnteredName] = useState("");
   const [enteredNameTouched, setEnteredNameTouched] = useState(false);
-  const [formIsValid, setFormIsValid] = useState(false);
 
   // as enteredNameIsValid can derived from enteredName
   const enteredNameIsValid = enteredName.trim() !== "";
   const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
+  let formIsValid = false;
 
-  useEffect(() => {
-    if (enteredNameIsValid) {
-      setFormIsValid(true);
-    } else {
-      setFormIsValid(false);
-    }
-  }, [enteredNameIsValid]);
+  if (enteredNameIsValid) {
+    formIsValid = true;
+  }
 
   const nameInputChangeHandler = (event) => {
     setEnteredName(event.target.value);

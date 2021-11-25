1e9f7a289dba04378d6141c38f5029deef4da3d7
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2021-11-25 12:26:48 -0500

    12-1   add validation for name Input
    Pay attention to the snameChangeHandler, if I update the state after trim the input value, then the last entered letter will be there without updated on time.
    
    can uncomment the line the re-create the issue.

--- src/components/BasicForm.js
+++ src/components/BasicForm.js
@@ -1,21 +1,66 @@
+import { useState } from "react";
+
 const BasicForm = (props) => {
+  console.log("basic from run !");
+
+  const [enteredName, setEnteredName] = useState("");
+  const [isNameTouched, setNameTouched] = useState(false);
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    setNameTouched(true);
+  };
+
+  // Here find a issue, if don't comment the trim() line
+  // then the last letter cannot be deleted
+  // as when we delete the last letter, the latter in the memory has been deleted
+  // while as we have the trim().length logic, it will not trigger the setState again
+  // so that the component will not be rendered, so that the last letter is always be there
+
+  const nameChangeHandler = (event) => {
+    setNameTouched(true);
+    //if (event.target.value.trim().length !== 0) {
+    setEnteredName(event.target.value.trim());
+    // }
+  };
+  const nameBlueHandler = (event) => {
+    setNameTouched(true);
+    if (event.target.value.trim().length !== 0) {
+      isNameValid = true;
+    }
+  };
+  let isNameValid = false;
+  if (isNameTouched && enteredName.trim().length !== 0) {
+    isNameValid = true;
+  } else if (!isNameTouched && enteredName.trim().length === 0) {
+    isNameValid = true;
+  }
+
+  let nameStyle = isNameValid ? "form-control" : "form-control invalid";
   return (
-    <form>
-      <div className='control-group'>
-        <div className='form-control'>
-          <label htmlFor='name'>First Name</label>
-          <input type='text' id='name' />
+    <form onSubmit={submitHandler}>
+      <div className="form-control">
+        <div className={nameStyle}>
+          <label htmlFor="name">First Name</label>
+          <input
+            type="text"
+            id="name"
+            onChange={nameChangeHandler}
+            onBlur={nameBlueHandler}
+            value={enteredName}
+          />
+          {!isNameValid && <p className="error-text">Name cannot be empty.</p>}
         </div>
-        <div className='form-control'>
-          <label htmlFor='name'>Last Name</label>
-          <input type='text' id='name' />
+        <div className="form-control">
+          <label htmlFor="name">Last Name</label>
+          <input type="text" id="name" />
         </div>
       </div>
-      <div className='form-control'>
-        <label htmlFor='name'>E-Mail Address</label>
-        <input type='text' id='name' />
+      <div className="form-control">
+        <label htmlFor="name">E-Mail Address</label>
+        <input type="text" id="name" />
       </div>
-      <div className='form-actions'>
+      <div className="form-actions">
         <button>Submit</button>
       </div>
     </form>
